93cd0883f2bce8d849219d69710fd526816795d9
David Wong <davidwg@calibra.com>
2020-03-24 12:50:03 -0700

    new ways of finding dependencies should work

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,10 +1,16 @@
-use std::collections::{HashSet, hash_map::{HashMap, Entry}};
-use std::iter::FromIterator;
-use std::path::{PathBuf};
-use guppy::graph::{DependencyDirection, PackageGraph, PackageMetadata, DependencyLink};
+use guppy::graph::{DependencyDirection, DependencyLink, PackageGraph, PackageMetadata};
 use guppy::{MetadataCommand, PackageId};
 use regex::Regex;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+use std::collections::{
+  hash_map::{Entry, HashMap},
+  HashSet,
+};
+use std::ffi::OsStr;
+use std::fs;
+use std::iter::FromIterator;
+use std::path::Path;
+use std::path::PathBuf;
 use tempdir::TempDir;
 
 //
@@ -15,51 +21,38 @@ use tempdir::TempDir;
 /// PackageRisk contains information about a package after analysis.
 #[derive(Default, Serialize, Deserialize)]
 pub struct PackageRisk {
-
   // metadata
   // --------
-
   /// name of the dependency
-  pub name: String,              
+  pub name: String,
   /// potentially different versions are pulled (bad)
-  pub versions: HashSet<String>, 
+  pub versions: HashSet<String>,
   /// link to its repository
-  pub repo: Option<String>,      
-
+  pub repo: Option<String>,
   // useful for analysis
   // -------------------
-
   /// path to the actual source code on disk
   #[serde(skip)]
-  pub manifest_path: PathBuf,     
-
+  pub manifest_path: PathBuf,
   // analysis result
   // ---------------
-
   /// transitive dependencies (in reversed direction)
   pub transitive_dependencies: HashSet<PackageId>,
-
   /// total number of transitive third party dependencies imported
   /// by this dependency (not including this dependency)
   pub total_third_deps: u64,
-
   /// number of root crates that import this package
   pub root_importers: Vec<PackageId>,
-
   // TODO: implement this
   /// total number of transitive third party dependencies imported
   /// by this dependency, and only by this dependency
   pub exclusive_deps_introduced: Vec<PackageId>,
-
   /// number of non-rust lines-of-code
   pub non_rust_loc: u64,
-
   /// number of rust lines-of-code
   pub rust_loc: u64,
-
   /// number of lines of unsafe code
   pub unsafe_loc: u64,
-
   /// number of github stars, if any
   pub stargazers_count: u64,
 }
@@ -76,17 +69,16 @@ struct GithubResponse {
   stargazers_count: u64,
 }
 impl GithubResponse {
-
   fn get_github_stars(
     github_client: reqwest::blocking::Client,
     github_token: Option<(&str, &str)>,
-    repo: &str
+    repo: &str,
   ) -> Option<u64> {
     // is this a github repo?
     let re = Regex::new(r"github\.com/([a-zA-Z0-9._-]*/[a-zA-Z0-9._-]*)").unwrap();
     let caps = re.captures(repo);
     caps
-      .and_then(|caps| caps.get(1) )
+      .and_then(|caps| caps.get(1))
       // yes it is a github repo
       .and_then(|repo| {
         // create request to github API
@@ -122,28 +114,45 @@ impl GithubResponse {
   }
 }
 
-fn get_root_importers(package_graph: &PackageGraph, root_crates: &HashSet<&PackageId>, dependency: &PackageId) -> Vec<PackageId> {
-  let root_importers = package_graph.select_reverse(std::iter::once(dependency)).unwrap();
+fn get_root_importers(
+  package_graph: &PackageGraph,
+  root_crates: &HashSet<&PackageId>,
+  dependency: &PackageId,
+) -> Vec<PackageId> {
+  let root_importers = package_graph
+    .select_reverse(std::iter::once(dependency))
+    .unwrap();
   let root_importers = root_importers.into_iter_metadatas(Some(DependencyDirection::Reverse));
   let root_importers: Vec<&PackageMetadata> = root_importers
     .filter(|pkg_metadata| root_crates.contains(&pkg_metadata.id())) // a root crate is an importer
     .collect();
-  let root_importers = root_importers.iter().map(|pkg_metadata| pkg_metadata.id().clone()).collect();
+  let root_importers = root_importers
+    .iter()
+    .map(|pkg_metadata| pkg_metadata.id().clone())
+    .collect();
   root_importers
 }
 
-fn get_exclusive_deps(package_graph: &PackageGraph, root_crates: &HashSet<&PackageId>, dependency: &PackageId) -> Vec<PackageId> {
+fn get_exclusive_deps(
+  package_graph: &PackageGraph,
+  root_crates: &HashSet<&PackageId>,
+  dependency: &PackageId,
+) -> Vec<PackageId> {
   // get all the transitive dependencies of `dependency`
-  let transitive_deps = package_graph.select_forward(std::iter::once(dependency)).unwrap();
+  let transitive_deps = package_graph
+    .select_forward(std::iter::once(dependency))
+    .unwrap();
   let transitive_deps = transitive_deps.into_iter_ids(Some(DependencyDirection::Forward));
   // re-create a graph without edges leading to our dependency (and its tree)
   let mut package_graph = package_graph.clone();
-  package_graph.retain_edges(|_, dep_link| {
-    !(dep_link.to.id() == dependency)
-  });
+  package_graph.retain_edges(|_, dep_link| !(dep_link.to.id() == dependency));
   // obtain all dependencies from the root_crates
-  let new_all = package_graph.select_forward(root_crates.iter().copied()).unwrap();
-  let new_all: Vec<_> = new_all.into_iter_ids(Some(DependencyDirection::Forward)).collect();
+  let new_all = package_graph
+    .select_forward(root_crates.iter().copied())
+    .unwrap();
+  let new_all: Vec<_> = new_all
+    .into_iter_ids(Some(DependencyDirection::Forward))
+    .collect();
   // check if the original transitive dependencies are in there
   let mut exclusive_deps = Vec::new();
   for transitive_dep in transitive_deps {
@@ -159,29 +168,13 @@ fn get_exclusive_deps(package_graph: &PackageGraph, root_crates: &HashSet<&Packa
   exclusive_deps
 }
 
-// TODO: I get really bad results with this right now because some of the dependencies are inside the package
-// TODO: I should really just use geiger's technique to count the LOCs
-fn get_loc(package_risk: &mut PackageRisk) {
-  let package_path = package_risk.manifest_path.parent().unwrap();
-  // TODO: use WalkParallel
-  let walker = ignore::WalkBuilder::new(package_path).build();
-  for result in walker {
-    let file = result.unwrap();
-    if !file.file_type().unwrap().is_file() {
-      continue; // TODO: we ignore symlink here, do we want this? (maybe using canonicalize will fix this)
-    }
-    let filepath = match file.path().to_str() {
-      Some(x) => x,
-      None => {
-        eprintln!("couldn't convert the path to string {:?}", file.path());
-        return;
-      }
-    };
-
+// count the lines-of-code of all the given files
+fn get_loc(package_risk: &mut PackageRisk, dependency_files: &HashSet<String>) {
+  for dependency_file in dependency_files {
     // look for all lines of code (not just rust)
-    let lang = loc::lang_from_ext(filepath);
+    let lang = loc::lang_from_ext(dependency_file);
     if lang != loc::Lang::Unrecognized {
-      let count = loc::count(filepath);
+      let count = loc::count(dependency_file);
       // update LOC
       package_risk.non_rust_loc += u64::from(count.code);
       if lang == loc::Lang::Rust {
@@ -191,9 +184,100 @@ fn get_loc(package_risk: &mut PackageRisk) {
   }
 }
 
-fn analyze_unsafe(package_risk: &mut PackageRisk) {
-  //let rs_files = crate::crate_files::get_rs_files(package_risk.name.clone(), &package_risk.manifest_path);
+// count the unsafe lines-of-code of all the given rust files
+fn get_unsafe(package_risk: &mut PackageRisk, dependency_files: &HashSet<String>) {
+  for dependency_file in dependency_files {
+    let dependency_path = Path::new(dependency_file);
+    if dependency_path.extension() != Some(OsStr::new(".rs")) {
+      continue;
+    }
 
+    // TODO: is this the right way to count unsafe?
+    if let Ok(res) = geiger::find_unsafe_in_file(dependency_path, geiger::IncludeTests::No) {
+      // update
+      let mut unsafe_loc = res.counters.functions.unsafe_;
+      unsafe_loc += res.counters.exprs.unsafe_;
+      unsafe_loc += res.counters.item_impls.unsafe_;
+      unsafe_loc += res.counters.item_traits.unsafe_;
+      unsafe_loc += res.counters.methods.unsafe_;
+      package_risk.unsafe_loc += unsafe_loc;
+    }
+  }
+}
+
+// parse the dep-info files that contain all the files relevant to the compilation of a dependency (these files are like Makefiles)
+// (minus libraries linked via bindings)
+// TODO: what to do about them?
+fn parse_rustc_dep_info(rustc_dep_info: &Path) -> HashSet<String> {
+  let contents = fs::read_to_string(rustc_dep_info).unwrap();
+  // inspired from https://github.com/rust-lang/cargo/blob/13cd4fb1e8be5b8fb44008052cf31a839d745a45/src/cargo/core/compiler/fingerprint.rs#L1646
+  let mut dependency_files = HashSet::new();
+  for line in contents.lines() {
+    if let Some(pos) = line.find(": ") {
+      let _target = &line[..pos];
+      let mut deps = line[pos + 2..].split_whitespace();
+      while let Some(file) = deps.next() {
+        let mut file = file.to_string();
+        while file.ends_with('\\') {
+          file.pop();
+          file.push(' ');
+          //file.push_str(deps.next().ok_or_else(|| {
+          //internal("malformed dep-info format, trailing \\".to_string())
+          //})?);
+          file.push_str(deps.next().expect("malformed dep-info format, trailing \\"));
+        }
+        dependency_files.insert(file);
+      }
+    }
+  }
+  dependency_files
+}
+
+// retrieve every single file in the dependency
+fn get_every_file_in_folder(package_path: &Path) -> HashSet<String> {
+  let mut dependency_files = HashSet::new();
+  let walker = ignore::WalkBuilder::new(package_path).build();
+  for result in walker {
+    let file = result.unwrap();
+    // TODO: we ignore symlink here, do we want this? (we could canonicalize)
+    if !file.file_type().unwrap().is_file() {
+      continue;
+    }
+    match file.path().to_str() {
+      Some(filepath) => dependency_files.insert(filepath.to_string()),
+      None => {
+        eprintln!("couldn't convert the path to string {:?}", file.path());
+        continue;
+      }
+    };
+  }
+  //
+  dependency_files
+}
+
+fn get_dependency_files(package_risk: &mut PackageRisk, target_dir: &Path) -> HashSet<String> {
+  use glob::glob;
+
+  // find the dep-info file for that dependency
+  let mut dep_files_path = target_dir.to_path_buf();
+  dep_files_path.push("debug/deps");
+  let without_underscore_name = package_risk.name.clone().replace("-", "_");
+  let dependency_file = format!("{}-*.d", without_underscore_name);
+  dep_files_path.push(dependency_file);
+  println!("debug: {:?}", dep_files_path);
+  let dep_files_path = glob(dep_files_path.to_str().unwrap()).unwrap().next();
+  match dep_files_path {
+    // we found a dep-info file
+    Some(glob_result) => {
+      let dep_files_path = glob_result.unwrap();
+      parse_rustc_dep_info(dep_files_path.as_path())
+    }
+    // we didn't find a dep-info file, let's do it the old fashion way
+    None => {
+      let package_path = package_risk.manifest_path.parent().unwrap();
+      get_every_file_in_folder(package_path)
+    }
+  }
 }
 
 //
@@ -201,16 +285,23 @@ fn analyze_unsafe(package_risk: &mut PackageRisk) {
 // ------
 //
 
-fn create_or_update_dependency(analysis_result: &mut HashMap<PackageId, PackageRisk>, dep_link: &DependencyLink) {
+fn create_or_update_dependency(
+  analysis_result: &mut HashMap<PackageId, PackageRisk>,
+  dep_link: &DependencyLink,
+) {
   match analysis_result.entry(dep_link.to.id().to_owned()) {
     Entry::Occupied(mut entry) => {
       let package_risk = entry.get_mut();
-      package_risk.versions.insert(dep_link.to.version().to_string());
+      package_risk
+        .versions
+        .insert(dep_link.to.version().to_string());
     }
     Entry::Vacant(entry) => {
       let mut package_risk = PackageRisk::default();
       package_risk.name = dep_link.to.name().to_owned();
-      package_risk.versions.insert(dep_link.to.version().to_string());
+      package_risk
+        .versions
+        .insert(dep_link.to.version().to_string());
       package_risk.repo = dep_link.to.repository().map(|x| x.to_owned());
       package_risk.manifest_path = dep_link.to.manifest_path().to_path_buf();
       entry.insert(package_risk);
@@ -241,7 +332,6 @@ pub fn analyze_repo(
   github_token: Option<(&str, &str)>,
   to_ignore: Option<Vec<&str>>,
 ) -> Result<(HashSet<PackageId>, HashMap<PackageId, PackageRisk>), String> {
-
   // Obtain package graph via guppy
   // ------------------------------
   //
@@ -266,13 +356,13 @@ pub fn analyze_repo(
   // and remove workspace crates that we want to ignore
   if let Some(to_ignore) = to_ignore {
     root_crates_to_analyze = root_crates_to_analyze
-    .into_iter()
-    .filter(|pkg_id| {
-      let package_metadata = package_graph.metadata(pkg_id).unwrap();
-      let package_name = package_metadata.name();
-      !to_ignore.contains(&package_name)
-    })
-    .collect();
+      .into_iter()
+      .filter(|pkg_id| {
+        let package_metadata = package_graph.metadata(pkg_id).unwrap();
+        let package_name = package_metadata.name();
+        !to_ignore.contains(&package_name)
+      })
+      .collect();
   }
 
   // What dependencies do we want to analyze?
@@ -323,42 +413,26 @@ pub fn analyze_repo(
   // ---------------------------------------------
   //
 
-  // cargo clean
-  /*
-  std::process::Command::new("cargo")
-    .args(&[
-      "clean", 
-      "--manifest-path", 
-      manifest_path,
-      "-q"
-    ])
-    .output()
-    .expect("failed to clean crate");
-  */
-
-  // cargo build
   let target_dir = TempDir::new("target_dir").expect("could not create temporary folder");
+  let target_dir = target_dir.path();
   println!("target-dir: {:?}", target_dir);
   let a = std::process::Command::new("cargo")
     .args(&[
-      "build", 
-      "--manifest-path", 
+      "build",
+      "--manifest-path",
       manifest_path,
       "--target-dir",
-      target_dir.path().to_str().unwrap(),
-      "-q"
+      target_dir.to_str().unwrap(),
+      "-q",
     ])
     .output()
     .expect("failed to build crate");
-
-  // clean temporary folders
 
   // Analyze!
   // --------
   //
 
   for (package_id, mut package_risk) in analysis_result.iter_mut() {
-
     // .transitive_dependencies
     package_risk.transitive_dependencies = package_graph
       .select_forward(std::iter::once(package_id))
@@ -387,14 +461,14 @@ pub fn analyze_repo(
     package_risk.root_importers = root_importers;
 
     // .exclusive_deps_introduced
-    let exclusive_deps_introduced = get_exclusive_deps(&package_graph, &root_crates_to_analyze, package_id);
+    let exclusive_deps_introduced =
+      get_exclusive_deps(&package_graph, &root_crates_to_analyze, package_id);
     package_risk.exclusive_deps_introduced = exclusive_deps_introduced;
 
-    // .non_rust_loc + .rust_loc
-    get_loc(&mut package_risk);
-
-    // .unsafe_loc
-    analyze_unsafe(&mut package_risk);
+    // .non_rust_loc + .rust_loc + unsafe_loc
+    let dependency_files = get_dependency_files(&mut package_risk, &target_dir);
+    get_loc(&mut package_risk, &dependency_files);
+    get_unsafe(&mut package_risk, &dependency_files);
 
     // .stargazers_count
     // TODO: also retrieve latest SHA commit (of release)
@@ -410,4 +484,3 @@ pub fn analyze_repo(
   //
   Ok((main_dependencies, analysis_result))
 }
-
